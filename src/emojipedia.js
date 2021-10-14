2d0bfb55efbbd6e6dd27b6ce7991ff203844a95d
Anuradha Dixit <82029747+AnuradhaDixit@users.noreply.github.com>
2021-10-14 12:14:50 +0530

    it is a familypedia

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -1,24 +1,54 @@
 const emojipedia = [
   {
     id: 1,
-    emoji: "💪",
-    name: "Tense Biceps",
+    emoji: "😎",
+    name: "Anuradha",
     meaning:
-      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+      "Anuradha is a Hindi name for girls meaning Goddess of good luck and it is also a Name of a star."
   },
   {
     id: 2,
-    emoji: "🙏",
-    name: "Person With Folded Hands",
+    emoji: "🧑",
+    name: "Kapil",
     meaning:
-      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+      ":Name of a sage, The Sun, Fire, Another name of Lord Vishnu, An incarnation of Vishnu"
   },
   {
     id: 3,
-    emoji: "🤣",
-    name: "Rolling On The Floor, Laughing",
+    emoji: "👧",
+    name: "Kirty",
+    meaning: "A submission from India says the name Kirty means 'Fame, Glory' "
+  },
+
+  {
+    id: 4,
+    emoji: "👦",
+    name: "Anu",
     meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+      "Anu, Anum, also called An (Sumerian: 𒀭 AN, from 𒀭 an “Sky”, “Heaven”), is the divine personification of the sky, supreme god, and ancestor of all the deities in ancient Mesopotamian religion."
+  },
+
+  {
+    id: 5,
+    emoji: "👵",
+    name: "Anjali",
+    meaning:
+      "Anjali is a Sanskrit word that means divine offering. It is not only a given name, but also the name given to the greeting between Hindus, Buddhists and other religions on the Indian subcontinent: hands folded together. "
+  },
+
+  {
+    id: 6,
+    emoji: "👨",
+    name: "Rajesh",
+    meaning: "God of kings, Lord of Kings, Ruler of Kings. "
+  },
+
+  {
+    id: 7,
+    emoji: "👩",
+    name: "Suman",
+    meaning:
+      "Means well-disposed, good mind, derived from the Sanskrit prefix सु (su) meaning good combined with मनस् (manas) meaning mind"
   }
 ];
 
